0e3eee90249df01accbe400a5f568d741416bda9
G02m0n1k <103603495+G02m0n1k@users.noreply.github.com>
2024-09-15 16:51:35 +0300

    added source code

---
+++ app/src/main/java/tb/g02m0n1k/twitblitlite/MainActivity.java
@@ -0,0 +1,108 @@
+package tb.g02m0n1k.twitblitlite;
+
+import static tb.g02m0n1k.twitblitlite.R.drawable.gradient_bar;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private static final String TB = "twitblit.ru"; // URL Twitblit
+    private static final String TBL = "https://" + TB + "/login"; // URL Twitblit для входа
+    Button reloadButton;
+    TextView errpage;
+    View bg;
+    TextView textViewLink;
+
+    @SuppressLint({"SetJavaScriptEnabled", "MissingInflatedId", "ResourceType"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        // Окрашивание строки состояния в градиент
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, android.R.color.transparent));
+        getWindow().setBackgroundDrawableResource(gradient_bar);
+
+        webView = findViewById(R.id.webView);
+        bg = findViewById(R.id.bg);
+        errpage = findViewById(R.id.errpage);
+        reloadButton = findViewById(R.id.reloadButton);
+        textViewLink = findViewById(R.id.sourcecode);
+
+        // Первоначальная загрузка страницы TBL
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(TBL);
+
+        reloadButton.setOnClickListener(v -> {
+            webView.loadUrl(TBL);
+            errpage.setVisibility(View.GONE);
+            reloadButton.setVisibility(View.GONE);
+            textViewLink.setVisibility(View.GONE);
+            bg.setVisibility(View.GONE);
+        });
+
+        textViewLink.setOnClickListener(v -> {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://gitverse.ru/G02m0n1k/Twitblit_LITE"));
+            startActivity(browserIntent);
+        });
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @SuppressWarnings("deprecation") @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @SuppressLint("ObsoleteSdkInt")
+            @TargetApi(Build.VERSION_CODES.N) @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String host = request.getUrl().getHost();
+                if (host != null && host.matches("^" + TB + ".*")) {
+                    // Этот сайт не переопределять! Пусть WebView загрузит его
+                    return false;
+                }
+                // Иначе, ссылка не предназначена для страницы на сайте, поэтому запустить другую Activity, которая работает с URL-адресами
+                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                // Отобразить ошибку, если возникла ошибка интернет-соединения
+                bg.setVisibility(View.VISIBLE);
+                errpage.setVisibility(View.VISIBLE);
+                reloadButton.setVisibility(View.VISIBLE);
+                textViewLink.setVisibility(View.VISIBLE);
+            }
+        };
+        webView.setWebViewClient(webViewClient);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack(); // Возврат к предыдущей странице
+        } else {
+            super.onBackPressed(); // Закрытие приложения
+        }
+    }
+
+}
